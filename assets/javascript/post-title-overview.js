7458667f7e7565f3eedf169ffa373afe020d4e87
Frej <friggies@yahoo.com>
2023-01-12 18:35:18 +0100

    make overview links work

--- assets/javascript/post-title-overview.js
+++ assets/javascript/post-title-overview.js
@@ -2,7 +2,7 @@ let overview = document.getElementById("title-overview");
 let titles = Array.from(document.getElementsByTagName("h2"));
 titles.forEach(title => {
     let li = document.createElement("li");
-    li.innerHTML = "- <a href='#"+title.innerText.toLowerCase()+"'>"+title.innerText+"</a>";
+    li.innerHTML = "- <a href='#"+title.innerText.toLowerCase().replace(" ", "-")+"'>"+title.innerText+"</a>";
     if (overview != null) {
         overview.appendChild(li);
     }
