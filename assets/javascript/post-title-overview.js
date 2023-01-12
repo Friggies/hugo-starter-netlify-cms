5287a91aea081c2019eb93061eb30eabca099be3
Frej <friggies@yahoo.com>
2023-01-12 18:30:48 +0100

    post design and overview

---
+++ assets/javascript/post-title-overview.js
@@ -0,0 +1,9 @@
+let overview = document.getElementById("title-overview");
+let titles = Array.from(document.getElementsByTagName("h2"));
+titles.forEach(title => {
+    let li = document.createElement("li");
+    li.innerHTML = "- <a href='#"+title.innerText.toLowerCase()+"'>"+title.innerText+"</a>";
+    if (overview != null) {
+        overview.appendChild(li);
+    }
+});
